72577937533f9e293d55ef02a66b7a625ef6dc7c
Fauzi Nur Noviansyah <fnnoviansyah@gmail.com>
2024-06-06 14:35:55 +0700

    fix bug

---
+++ gameappsubmission/Utils/BackgroundColorUtil.swift
@@ -0,0 +1,8 @@
+//
+//  BackgroundColorUtil.swift
+//  gameappsubmission
+//
+//  Created by Fauzi Nur Noviansyah on 05/06/24.
+//
+
+import Foundation
---
+++ gameappsubmission/Utils/StringUtil.swift
@@ -0,0 +1,8 @@
+//
+//  StringUtil.swift
+//  gameappsubmission
+//
+//  Created by Fauzi Nur Noviansyah on 06/06/24.
+//
+
+import Foundation
